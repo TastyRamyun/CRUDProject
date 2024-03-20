f7cf979f887c152d6922e5a28ba2ca9510b540df
Enrique Perez <ep17@me.com>
2024-03-20 02:59:22 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngCRUDMaster';
+
+  constructor(private router: Router){}
+
+  goToDash(){
+    console.log("Button clicked");
+    this.router.navigate(['/dash']);
+  }
 }
